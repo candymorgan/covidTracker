7a3d891db96fab7d470956c610e38b9385b3b813
candy morgan <candyaz1000@gmail.com>
2022-07-13 22:56:31 +0100

    another commit

--- src/Components/TopAfectedCountries/index.jsx
+++ src/Components/TopAfectedCountries/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import './TopAfectedCountries.css'
+import us from '../../🇺🇸.svg'
+import it from '../../it.svg'
+import ca from '../../ca.svg'
+import es from '../../es.svg'
+import Rectangle34 from '../../Rectangle34.svg'
+
+
+
+
 const TopAfectedCountries = () => {
   return (
     <>
@@ -17,7 +26,9 @@ const TopAfectedCountries = () => {
                 <div className="countries">
                   <div className="country">
                     <div className="nameLogo_div">
-                      <div className="countryLogo1"></div>
+                      <div className="countryLogo1">
+                        <img src={us} alt="" />
+                      </div>
                       <div className="countryName1">USA</div>
                     </div>
                     <div className="figures1">1244355637</div>
@@ -25,31 +36,39 @@ const TopAfectedCountries = () => {
 
                   <div className="country">
                     <div className="nameLogo_div2">
-                      <div className="countryLogo2"></div>
+                      <div className="countryLogo2">
+                      <img src={it} alt="" />
+                      </div>
                       <div className="countryName2">Italy</div>
                     </div>
-                    <div className="figures2"></div>
+                    <div className="figures2">65637816273</div>
                   </div>
 
                   <div className="country">
                     <div className="nameLogo_div3">
-                      <div className="countryLogo3"></div>
+                      <div className="countryLogo3">
+                      <img src={ca} alt="" />
+                      </div>
                       <div className="countryName3">Canada</div>
                     </div>
-                    <div className="figures3"></div>
+                    <div className="figures3">65258485217</div>
                   </div>
 
                   <div className="country">
                     <div className="nameLogo_div4">
-                      <div className="countryLogo4"></div>
+                      <div className="countryLogo4">
+                      <img src={es} alt="" />
+                      </div>
                       <div className="countryName4">USA</div>
                     </div>
-                    <div className="figures4"></div>
+                    <div className="figures4">65258415278</div>
                   </div>
                 </div>
 
                 <div className="caseData">yyyyy</div>
-                <div className="worldmap">jjjjj</div>
+                <div className="worldmap">
+                  <img src={Rectangle34} alt="" />
+                </div>
               </div>
 
 
