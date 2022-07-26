5716ed45e420f27d7c5e4521af229156e411d779
candy morgan <candyaz1000@gmail.com>
2022-07-26 23:29:43 +0100

    ertyhhh

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -5,24 +5,29 @@ import logo1 from '../../logo1.svg'
 const Navbar = () => {
   return (
     <nav className="navcontainer">
-        <div className="covid19Strain">
-          <div className="covidLogo">
-            <img src={logo1} alt="" />
-          </div>
-          <div className="covidWriteUp"><h2><a href="">Covid19Strain</a></h2></div>
+      <div className="covid19Strain">
+        <div className="covidLogo">
+          <img src={logo1} alt="" />
         </div>
-        <div className="navLinks">
-          <ul>
-            <li className="home"><a href="">HOME</a></li>
-            <li><a href="">SYMPTOMS</a></li>
-            <li><a href="">PROTECTION</a></li>
-          </ul>
-        </div>
+        <div className="covidWriteUp"><h2><a href="">Covid19Strain</a></h2></div>
+      </div>
+      <div className="navLinks">
+        <ul>
+          <li className="home"><a href="">HOME</a></li>
+          <li><a href="">SYMPTOMS</a></li>
+          <li><a href="">PROTECTION</a></li>
+        </ul>
+      </div>
 
-        <div className="date">
-          <p>Updated: May 22, 2020</p>
-        </div>
-      </nav>
+      <div className="date">
+        <p>Updated: May 22, 2020</p>
+      </div>
+      {/* <svg viewBox="0 0 100 80" width="40" height="40">
+        <rect width="100" height="20"></rect>
+        <rect y="30" width="100" height="20"></rect>
+        <rect y="60" width="100" height="20"></rect>
+      </svg> */}
+    </nav>
   )
 }
 
